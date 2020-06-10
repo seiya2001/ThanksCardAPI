623efd29f3ebeeed5a52a9dd8098027ec4138819
Reishin-Sakuma <reisin@occ.co.jp>
2020-06-10 16:10:59 +0900

    返事機能実装！！！

--- Controllers/ThanksCardController.cs
+++ Controllers/ThanksCardController.cs
@@ -46,5 +46,62 @@ namespace ThanksCardAPI.Controllers
             // TODO: Error Handling
             return thanksCard;
         }
+
+
+
+
+        // PUT: api/Users/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutThanksCard(long id, ThanksCard thanksCard)
+        {
+            if (id != thanksCard.Id)
+            {
+                return BadRequest();
+            }
+
+            // Department には既に存在しているユーザが入るため、更新の対象から外す。
+            //_context.Departments.Attach(user.Department);
+
+            _context.Entry(thanksCard).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ThanksCardExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/Users/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ThanksCard>> DeleteThanksCard(long id)
+        {
+            var thanksCard = await _context.ThanksCards.FindAsync(id);
+            if (thanksCard == null)
+            {
+                return NotFound();
+            }
+
+            _context.ThanksCards.Remove(thanksCard);
+            await _context.SaveChangesAsync();
+
+            return thanksCard;
+        }
+
+        private bool ThanksCardExists(long id)
+        {
+            return _context.ThanksCards.Any(e => e.Id == id);
+        }
     }       
 }
