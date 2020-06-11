41d4e9806823f803e43b12e05683ae6a9766b173
seiya2001 <seiya-t@occ.co.jp>
2020-06-11 12:03:11 +0900

    a

---
+++ Migrations/20200610011638_SettingDatabase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ThanksCardAPI.Migrations
+{
+    public partial class SettingDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+
+        }
+    }
+}
